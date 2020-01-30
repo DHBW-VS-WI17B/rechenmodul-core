a6565552e738c7a2091624b8179e22ab6c3253de
skyfrk <simon@esslinger.it>
2020-01-31 00:01:17 +0100

    chore: setup main.ts

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { calcCorrelationCoefficient } from './calcCorrelationCoefficient'
+import { calcCovariance } from './calcCovariance'
+import { calcVariance } from './calcVariance'
+import { calcOneDimensionalMean } from './calcOneDimensionalMean'
+import { calcRegressionGraph } from './calcRegressionGraph'
+import { validatePointList } from './validatePointList'
+
+module.exports = {
+  calcCorrelationCoefficient,
+  calcCovariance,
+  calcVariance,
+  calcOneDimensionalMean,
+  calcRegressionGraph,
+  validatePointList,
+}
