d89dff85cd5c63d4089206859ac6840b24781d0c
Simon Eßlinger <simon@esslinger.it>
2020-01-30 12:28:25 +0100

    test: add regression graph test

--- tests/calc.test.ts
+++ tests/calc.test.ts
@@ -166,3 +166,46 @@ test("gets the correlation coefficient -0.0574704 for the given list of points",
 
   expect(result).toBeCloseTo(-0.0574704);
 });
+
+test("gets the regression graph for the given list of points", () => {
+  let calc = new Calc();
+
+  let points = [
+    new Point(1, 0),
+    new Point(1, 0),
+    new Point(1, 1),
+    new Point(1, 1),
+    new Point(1, 1),
+    new Point(1, 4),
+    new Point(0, 3),
+    new Point(0, 3),
+    new Point(0, 3),
+    new Point(2, 1),
+    new Point(2, 1),
+    new Point(2, 1),
+    new Point(2, 1),
+    new Point(2, 1),
+    new Point(2, 3),
+    new Point(2, 5),
+    new Point(2, 5),
+    new Point(3, 2),
+    new Point(3, 4),
+    new Point(3, 4),
+    new Point(4, 1),
+    new Point(4, 1),
+    new Point(4, 3),
+    new Point(4, 4),
+    new Point(4, 4),
+    new Point(5, 0),
+    new Point(5, 0),
+    new Point(5, 0),
+    new Point(5, 1),
+    new Point(5, 5)
+  ];
+
+  let result = calc.GetRegressionGraph(points);
+
+  expect(result.incline).toBeCloseTo(-0.1586206);
+  expect(result.quality).toBeCloseTo(0.00043357);
+  expect(result.yAxisSection).toBeCloseTo(-0.05938);
+});
