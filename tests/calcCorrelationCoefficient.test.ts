5776ab731c3808ab15dde119a02702500121812d
Simon Eßlinger <simon@esslinger.it>
2020-01-30 16:38:42 +0100

    refactor: move test points to testData.ts

--- tests/calcCorrelationCoefficient.test.ts
+++ tests/calcCorrelationCoefficient.test.ts
@@ -1,39 +1,6 @@
-import { Point } from "../src/entities/Point";
 import { calcCorrelationCoefficient } from "../src/calcCorrelationCoefficient";
+import { points } from "./utils/testData";
 
 test("calculates the correlation coefficient for the given list of points", async () => {
-  let points = [
-    new Point(1, 0),
-    new Point(1, 0),
-    new Point(1, 1),
-    new Point(1, 1),
-    new Point(1, 1),
-    new Point(1, 4),
-    new Point(0, 3),
-    new Point(0, 3),
-    new Point(0, 3),
-    new Point(2, 1),
-    new Point(2, 1),
-    new Point(2, 1),
-    new Point(2, 1),
-    new Point(2, 1),
-    new Point(2, 3),
-    new Point(2, 5),
-    new Point(2, 5),
-    new Point(3, 2),
-    new Point(3, 4),
-    new Point(3, 4),
-    new Point(4, 1),
-    new Point(4, 1),
-    new Point(4, 3),
-    new Point(4, 4),
-    new Point(4, 4),
-    new Point(5, 0),
-    new Point(5, 0),
-    new Point(5, 0),
-    new Point(5, 1),
-    new Point(5, 5)
-  ];
-
   expect(await calcCorrelationCoefficient(points)).toBeCloseTo(-0.0574704);
 });
--- tests/calcVariance.test.ts
+++ tests/calcVariance.test.ts
@@ -1,40 +1,7 @@
-import { Point } from "../src/entities/Point";
 import { calcVariance } from "../src/calcVariance";
+import { points } from "./utils/testData";
 
 test("calculates the variance for the given list of points", async () => {
-  let points = [
-    new Point(1, 0),
-    new Point(1, 0),
-    new Point(1, 1),
-    new Point(1, 1),
-    new Point(1, 1),
-    new Point(1, 4),
-    new Point(0, 3),
-    new Point(0, 3),
-    new Point(0, 3),
-    new Point(2, 1),
-    new Point(2, 1),
-    new Point(2, 1),
-    new Point(2, 1),
-    new Point(2, 1),
-    new Point(2, 3),
-    new Point(2, 5),
-    new Point(2, 5),
-    new Point(3, 2),
-    new Point(3, 4),
-    new Point(3, 4),
-    new Point(4, 1),
-    new Point(4, 1),
-    new Point(4, 3),
-    new Point(4, 4),
-    new Point(4, 4),
-    new Point(5, 0),
-    new Point(5, 0),
-    new Point(5, 0),
-    new Point(5, 1),
-    new Point(5, 5)
-  ];
-
   let result = await calcVariance(points);
 
   expect(result.x).toBeCloseTo(2.67124);
