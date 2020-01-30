407e8f9fb008a8196c78d7251856f5005c4b838b
Simon Eßlinger <simon@esslinger.it>
2020-01-30 12:06:03 +0100

    test: add test for variance

--- tests/calc.test.ts
+++ tests/calc.test.ts
@@ -42,3 +42,45 @@ test("gets the one dimensional mean P(2,53333...|2,586206897) for the given list
   expect(result.x).toBeCloseTo(2.53333333);
   expect(result.y).toBeCloseTo(2.1);
 });
+
+test("gets the variance P(2.67124|2.85172) for the given list of points", () => {
+  let calc = new Calc();
+
+  let points = [
+    new Point(1, 0),
+    new Point(1, 0),
+    new Point(1, 1),
+    new Point(1, 1),
+    new Point(1, 1),
+    new Point(1, 4),
+    new Point(0, 3),
+    new Point(0, 3),
+    new Point(0, 3),
+    new Point(2, 1),
+    new Point(2, 1),
+    new Point(2, 1),
+    new Point(2, 1),
+    new Point(2, 1),
+    new Point(2, 3),
+    new Point(2, 5),
+    new Point(2, 5),
+    new Point(3, 2),
+    new Point(3, 4),
+    new Point(3, 4),
+    new Point(4, 1),
+    new Point(4, 1),
+    new Point(4, 3),
+    new Point(4, 4),
+    new Point(4, 4),
+    new Point(5, 0),
+    new Point(5, 0),
+    new Point(5, 0),
+    new Point(5, 1),
+    new Point(5, 5)
+  ];
+
+  let result = calc.GetVariance(points);
+
+  expect(result.x).toBeCloseTo(2.67124);
+  expect(result.y).toBeCloseTo(2.85172);
+});
