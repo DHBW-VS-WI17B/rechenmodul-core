7aec96480e5fad86194875af3c28c18f88e9d3b5
Simon Eßlinger <simon@esslinger.it>
2020-01-30 11:18:15 +0100

    refactor: remove internalcalc

--- src/contracts/ICalc.ts
+++ src/contracts/ICalc.ts
@@ -8,4 +8,45 @@ export interface ICalc {
    * @returns calc Some mathemathical shit
    */
   Calc(Input: IPoint[]): Promise<IResult>;
+
+   /**
+   *
+   * @param points Points to validate
+   */
+  ValidatePointList(points: IPoint[]): void;
+
+  /**
+   *
+   * @param points List of points to calculate the one dimensional mean for
+   * @returns one dimensional mean
+   */
+  GetOneDimensionalMean(points: IPoint[]): number;
+
+  /**
+   *
+   * @param points List of points to calculcate the variance for
+   * @returns variance
+   */
+  GetVariance(points: IPoint[]): number;
+
+  /**
+   *
+   * @param points List of points to calculcate the covariance according to pearson algorythm for
+   * @returns covariance
+   */
+  GetCovariance(points: IPoint[]): number;
+
+  /**
+   *
+   * @param points List of points to calculcate the correlation coefficient for
+   * @returns correlation coefficient
+   */
+  GetCorrelationCoefficient(points: IPoint[]): number;
+
+  /**
+   *
+   * @param points List of points to calculcate the regression graph for
+   * @returns regression graph
+   */
+  GetRegressionGraph(points: IPoint[]): IRegressionGraph;
 }
