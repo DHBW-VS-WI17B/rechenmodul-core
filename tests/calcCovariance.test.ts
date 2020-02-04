c05a93f3cef07b85d87e0e75732944ce327e7637
Marvin.Rothmann <Marvin.Rothmann@ITNB19>
2020-02-04 14:42:10 +0100

    test: added tests for covariance

--- tests/calcCovariance.test.ts
+++ tests/calcCovariance.test.ts
@@ -1,6 +1,20 @@
 import { calcCovariance } from '../src/calcCovariance'
 import { points } from './utils/testData'
+import { Point } from '../src/entities/Point'
 
 test('calculates the covariance for the given list of points', async () => {
   expect(await calcCovariance(points)).toBeCloseTo(-0.15862)
 })
+
+
+test('calculates the covariance for low correlation set of points', async () => {
+  let pointsZero = [
+    new Point(5,5),
+    new Point(5,5),
+    new Point(5,5),
+    new Point(5,5)
+  ]
+  expect(await calcCovariance(pointsZero)).toBe(0)
+})
+
+
