c4bdff2ae406300236d7b7db1b9a0e5097dd50ae
skyfrk <simon@esslinger.it>
2020-02-05 12:26:24 +0100

    test: add fc test with floats for variance

--- tests/calcVariance.test.ts
+++ tests/calcVariance.test.ts
@@ -9,9 +9,24 @@ test('calculates the variance for the given list of points', async () => {
   expect(result.y).toBeCloseTo(2.85172)
 })
 
-test('the variance is always at least 0', async () => {
+test('the variance is always at least 0 (integers)', async () => {
   fc.assert(
     fc.asyncProperty(fc.array(fc.record({ x: fc.integer(), y: fc.integer() })), async points => {
+      if (points.length > 1) {
+        const variance = await calcVariance(points)
+        expect(variance.x).toBeGreaterThanOrEqual(0)
+        expect(variance.y).toBeGreaterThanOrEqual(0)
+      }
+    }),
+    {
+      seed: 4815162342,
+    }
+  )
+})
+
+test('the variance is always at least 0 (floats)', async () => {
+  fc.assert(
+    fc.asyncProperty(fc.array(fc.record({ x: fc.float(), y: fc.float() })), async points => {
       if (points.length > 1) {
         const variance = await calcVariance(points)
         expect(variance.x).toBeGreaterThanOrEqual(0)
