8da6334c99f5752389d2d0cb33fad0029a532fa1
skyfrk <simon@esslinger.it>
2020-02-05 12:00:06 +0100

    test: implement fc test for calcQuality

---
+++ tests/calcQuality.test.ts
@@ -0,0 +1,14 @@
+import { calcQuality } from '../src/calcQuality'
+import * as fc from 'fast-check'
+
+test.skip('the quality value is always between 0 and 1, including 0 and 1', async () => {
+  fc.assert(
+    fc.asyncProperty(fc.array(fc.record({ x: fc.integer(), y: fc.integer() })), async points => {
+      if (points.length > 1) {
+        const quality = await calcQuality(points)
+        expect(quality).toBeGreaterThanOrEqual(0)
+        expect(quality).toBeLessThanOrEqual(1)
+      }
+    })
+  )
+})
