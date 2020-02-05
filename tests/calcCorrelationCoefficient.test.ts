9662ae4719635177be24e154c6ec3d32f0b49acf
skyfrk <simon@esslinger.it>
2020-02-05 12:09:36 +0100

    test: add fc test for correlation coefficient

--- tests/calcCorrelationCoefficient.test.ts
+++ tests/calcCorrelationCoefficient.test.ts
@@ -1,6 +1,7 @@
 import { calcCorrelationCoefficient } from '../src/calcCorrelationCoefficient'
 import { points } from './utils/testData'
 import { Point } from '../src/entities/Point'
+import * as fc from 'fast-check'
 
 test('calculates the correlation coefficient for the given list of points', async () => {
   expect(await calcCorrelationCoefficient(points)).toBeCloseTo(-0.0574704)
@@ -31,4 +32,24 @@ test('calculates the correlation coefficient for high correlation points', async
 test('calculates the correlation coefficient for low correlation points', async () => {
   const pointsLow = [new Point(3, -3), new Point(2, -2), new Point(1, -1), new Point(0, 0)]
   expect(await calcCorrelationCoefficient(pointsLow)).toBe(-1)
+})
+
+test.skip('the correlation coefficient is always between -1 and 1, including -1 and 1', async () => {
+  fc.assert(
+    fc.asyncProperty(
+      fc.array(
+        fc.record({
+          x: fc.integer(),
+          y: fc.integer(),
+        })
+      ),
+      async points => {
+        if (points.length > 1) {
+          const correlationCoefficient = await calcCorrelationCoefficient(points)
+          expect(correlationCoefficient).toBeLessThanOrEqual(1)
+          expect(correlationCoefficient).toBeGreaterThanOrEqual(-1)
+        }
+      }
+    )
+  )
 })
